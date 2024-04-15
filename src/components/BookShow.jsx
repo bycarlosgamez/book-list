a34261a499969dd3f417a0159ab555cf58ddd321
Carlos Gámez <hardpx@gmail.com>
2024-04-15 10:12:04 -0600

    added edit book logic

--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
 function BookShow({ book, onDeleteBook }) {
+  const [showEdit, setShowEdit] = useState(false);
+
   const { title, id } = book;
 
-  const handleClick = () => {
+  const handleDeleteClick = () => {
     onDeleteBook(id);
   };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const content = !showEdit ? <h3>{title}</h3> : <BookEdit />;
+
   return (
     <div className='book-show'>
-      {title}
-      <div className='actioms'>
-        <button className='delete' onClick={handleClick}>
+      <div>{content}</div>
+      <div className='actions'>
+        <button className='edit' onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className='delete' onClick={handleDeleteClick}>
           Delete
         </button>
       </div>
