c8e979c51bee64fa23dd8ce018df11c6d94fd8ed
Carlos Gámez <hardpx@gmail.com>
2024-04-16 10:38:36 -0600

    updated edit and delete function to use json server and axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost/3001/books');
+
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   const addBook = async (title) => {
     const response = await axios.post('http://localhost:3001/books', {
@@ -15,14 +25,20 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const delateBookById = (id) => {
+  const delateBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => id !== book.id);
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
-      if (id === book.id) return { ...book, title: newTitle };
+      if (id === book.id) return { ...book, ...response.data };
 
       return book;
     });
